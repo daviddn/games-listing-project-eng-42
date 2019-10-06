7d0c9a66b61ec678ad5d887802a4e189edb857ad
David Naim <DNaim@spartaglobal.com>
2019-10-07 00:19:59 +0100

    created method to read all games from table

--- games_ad_listing_class.py
+++ games_ad_listing_class.py
@@ -6,9 +6,16 @@ class Games(Connectdb):
 
 # Define methods
 
-    # Shows one recipe in the recipe table
+    # Shows one game in the table
     def read_one(self, game):
-        query = self.filter_query(f"SELECT * FROM Recipes WHERE Game = '{game}'").fetchone()
+        query = self.filter_query(f"SELECT * FROM GamesList WHERE Game = '{game}'").fetchone()
         return query
 
-    
+    # Shows all games in the table
+    def read_all(self):
+        query = self.filter_query(f"SELECT * FROM GamesList")
+        while True:
+            record = query.fetchone()
+            if record is None:
+                break
+            print('GameAdID:', record[0], 'Game: ' + record[1], 'Seller: ' + record[2], 'Phone: ' + record[3], 'Price: ' + record[4], 'Position: ' + record[5], 'Latitude: ' + record[6], 'Longitude: ' + record[7])
---
+++ run.py
@@ -0,0 +1,7 @@
+from games_ad_listing_class import *
+
+db_games = Games()
+
+# print(db_games.read_one('FIFA 20'))
+
+db_games.read_all()
