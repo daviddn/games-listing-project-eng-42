9346dfa18a701707e204f35ed8d36ca9f6fd7a52
David Naim <DNaim@spartaglobal.com>
2019-10-06 23:36:48 +0100

    created db file to connect to sql database

---
+++ db_class.py
@@ -0,0 +1,16 @@
+import pyodbc
+import requests
+
+class Connectdb():
+    # have the characterstics to access the db
+    def __init__(self, server = 'localhost, 1433', database = 'SpartaGames', username = 'SA', password = 'Passw0rd2018'):
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+        self.conn_db = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
+        self.cursor = self.conn_db.cursor()
+
+    # Define methods
+    def filter_query(self, query):
+        return self.cursor.execute(query)
