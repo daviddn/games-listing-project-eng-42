0e52ba3f34f9dc8e975568a21e0a14ab1b05f270
David Naim <DNaim@spartaglobal.com>
2019-10-07 02:19:08 +0100

    created method to creat a game ad

--- games_ad_listing_class.py
+++ games_ad_listing_class.py
@@ -19,3 +19,8 @@ class Games(Connectdb):
             if record is None:
                 break
             print('GameAdID:', record[0], 'Game: ' + record[1], 'Seller: ' + record[2], 'Phone: ' + record[3], 'Price: ' + record[4], 'Position: ' + record[5], 'Latitude: ' + record[6], 'Longitude: ' + record[7])
+
+    # Creates new game ad in the table
+    def new(self, game, seller, phone, price, position, latitude, longitude):
+        self.filter_query(f"INSERT INTO GamesList (Game, Seller, Phone, Price, Position, Latitude, Longitude) VALUES {game, seller, phone, price, position, latitude, longitude}")
+        self.conn_db.commit()
--- run.py
+++ run.py
@@ -4,4 +4,6 @@ db_games = Games()
 
 # print(db_games.read_one('FIFA 20'))
 
-db_games.read_all()
+# db_games.read_all()
+
+# db_games.new('God of War', 'frensis97ferrari', '09182736450', '30', 'EC1V 0ES', '', '')
